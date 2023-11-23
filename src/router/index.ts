bee1804bcc6260774282e9752b1200644ffb3009
Yan <kahanov.yan@gmail.com>
2023-11-23 14:02:16 +0500

    init and clearned project

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../Pages/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('../views/AboutView.vue')
+    // }
+  ]
+})
+
+export default router
